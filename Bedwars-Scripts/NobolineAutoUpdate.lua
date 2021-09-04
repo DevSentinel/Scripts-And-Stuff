8a3dc7ff60c275192e23c00ce5a1a4b167a39663
DevSentinel <87673339+DevSentinel@users.noreply.github.com>
2021-09-04 19:12:22 -0400

    Update NobolineAutoUpdate.lua

--- Bedwars-Scripts/NobolineAutoUpdate.lua
+++ Bedwars-Scripts/NobolineAutoUpdate.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet'https://dogix.wtf/scripts/noboline/1c447d04-10ac-4fae-a010-8905d7fafb61')()
+loadstring(game:HttpGet'https://dogix.wtf/scripts/noboline/e256f847-5e9f-4319-9a27-17b308dd1319')()

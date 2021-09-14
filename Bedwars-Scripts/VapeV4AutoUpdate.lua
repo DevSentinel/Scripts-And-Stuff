f1b2316f894ef7c0380ba6bdf80e1c7d79f80982
DevSentinel <87673339+DevSentinel@users.noreply.github.com>
2021-09-14 09:49:32 -0400

    Update VapeV4AutoUpdate.lua

--- Bedwars-Scripts/VapeV4AutoUpdate.lua
+++ Bedwars-Scripts/VapeV4AutoUpdate.lua
@@ -1,2 +1,2 @@
--- Auto Updater For VAPE V4 BW
+-- Auto Updater For VAPE V4 BW (Replacing RBX Hub)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()

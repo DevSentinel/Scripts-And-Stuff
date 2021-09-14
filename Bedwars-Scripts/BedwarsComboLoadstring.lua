c5406cc603ed6cb54e420b23e8626ccfd4be96f7
DevSentinel <87673339+DevSentinel@users.noreply.github.com>
2021-09-14 09:51:00 -0400

    Update BedwarsComboLoadstring.lua

--- Bedwars-Scripts/BedwarsComboLoadstring.lua
+++ Bedwars-Scripts/BedwarsComboLoadstring.lua
@@ -1,2 +1,2 @@
--- Bed Wars Combo Script Loadstring, Ctrl + A To Copy Everything.
+-- Bed Wars Combo Script Loadstring, Ctrl + A To Copy Everything. (Dont Remove Anything, Including This.)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DevSentinel/Scripts-And-Stuff/main/Bedwars-Scripts/BedwarsCombo.lua"))()

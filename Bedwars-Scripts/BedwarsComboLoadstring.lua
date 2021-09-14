c387f4a73e6c131d568060c95b7e02b43420906f
DevSentinel <87673339+DevSentinel@users.noreply.github.com>
2021-09-14 09:47:50 -0400

    Create BedwarsComboLoadstring.lua

---
+++ Bedwars-Scripts/BedwarsComboLoadstring.lua
@@ -0,0 +1,2 @@
+-- Bed Wars Combo Script Loadstring, Ctrl + A To Copy Everything.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevSentinel/Scripts-And-Stuff/main/Bedwars-Scripts/BedwarsCombo.lua"))()
